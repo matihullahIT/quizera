d8270d83622873bfd38a2aa85f46c771defbd3f3
matihullahIT <matihurrehman.official@gmail.com>
2025-04-08 20:33:56 +0500

    feat: Update Layout component to allow horizontal overflow in page content

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -24,7 +24,7 @@ const Layout = ({ navbar, children, search }: LayoutProps) => {
         </div>
 
         {/* Page Content */}
-        <div className="flex-1 overflow-y-auto p-6 bg-gray-100">{children}</div>
+        <div className="flex-1 overflow-y-auto overflow-x-auto p-6 bg-gray-100">{children}</div>
       </div>
     </div>
   );
